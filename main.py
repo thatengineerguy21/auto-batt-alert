0f4a8755e990d85f006e08e2b4b87bc2506a3203
thatengineerguy13 <veduchaudhari441@gmail.com>
2024-03-05 06:11:58 +0530

    Version 0

---
+++ main.py
@@ -0,0 +1,29 @@
+import psutil
+from ctypes import Structure, windll, c_uint, sizeof, byref
+from time import sleep
+
+class LASTINPUTINFO(Structure):
+    _fields_ = [("cbSize", c_uint), ("dwTime", c_uint)]
+
+def get_idle_duration():
+    lii = LASTINPUTINFO()
+    lii.cbSize = sizeof(LASTINPUTINFO)
+    windll.user32.GetLastInputInfo(byref(lii))
+    millis = windll.kernel32.GetTickCount() - lii.dwTime
+    return millis / 1000.0
+
+def show_notification(title, message):
+    windll.user32.MessageBoxW(0, message, title, 1)
+
+def check_battery_level():
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+
+    if percent <= 20 or percent >= 80:
+        show_notification("Battery Alert", f"Battery level is {percent}%.")
+
+if __name__ == "__main__":
+    while True:
+        check_battery_level()
+        # Check every 10 minutes
+        sleep(600)
